39dcc58803f151af70e0250e7531e9081c98a90c
Brandon1212b <111893060+Brandon1212b@users.noreply.github.com>
2026-09-02 08:05:32 -0400

    Fix popup close: swallow leftover taps and toggle the same button

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as PopoverPrimitive from "@radix-ui/react-popover";
 
 import { cn } from "@/lib/utils";
+import { swallowBehindPopup } from "@/components/popup-dismiss-shield";
 
 function Popover({
   modal = true,
@@ -17,12 +18,20 @@ const PopoverAnchor = PopoverPrimitive.Anchor;
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+>(({ className, align = "center", sideOffset = 4, onPointerDownOutside, onInteractOutside, ...props }, ref) => (
   <PopoverPrimitive.Portal>
     <PopoverPrimitive.Content
       ref={ref}
       align={align}
       sideOffset={sideOffset}
+      onPointerDownOutside={(e) => {
+        swallowBehindPopup();
+        onPointerDownOutside?.(e);
+      }}
+      onInteractOutside={(e) => {
+        swallowBehindPopup();
+        onInteractOutside?.(e);
+      }}
       className={cn(
         "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-(--radix-popover-content-transform-origin)",
         className,
